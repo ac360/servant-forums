ad5314fd9503ac55eccc3bbc8f4ad61d2275b747
Austen Collins <austencollins@gmail.com>
2013-06-08 12:38:18 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServantForum::Application.config.secret_token = '08e8fce6515639e830dcb0c1d7dac3e40352048437043c203dab33eb790f83784952965034a1c1ab6f5e3135568f9153aa79262ea85b4f1318116108630d473c'
