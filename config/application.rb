573f9df6e3513a347354b5ee031a8c42db91cdac
Austen Collins <austencollins@gmail.com>
2013-06-09 16:30:10 +0200

    changes

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module ServantForum
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # MOnkey Patch for Forem Asset Precomiple Error
   end
