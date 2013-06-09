ad72bfaab3a2c21657779f5364b7c2353ff0f661
Austen Collins <austencollins@gmail.com>
2013-06-09 17:15:32 +0200

    changes

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,6 @@ module ServantForum
      #Add Folder for custom fonts
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
-    # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
